f67793ec4611254ad0751d7be1cab957aa9c3b05
yijian <yj.chong@vilor.com>
2025-06-28 17:52:14 +0800

    Remove logout button from app.vue to streamline user interface and improve layout consistency.

--- app/app.vue
+++ app/app.vue
@@ -24,13 +24,6 @@ function logout() {
   <div>
     <NuxtLayout>
       <NuxtPage />
-      <button
-        v-if="showLogout"
-        @click="logout"
-        class="fixed top-4 right-4 z-50 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded shadow-lg"
-      >
-        Logout
-      </button>
     </NuxtLayout>
   </div>
 </template>
