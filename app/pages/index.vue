bcb216feb96b2f4cdd68761b557c47b29711ab5e
yijian <yj.chong@vilor.com>
2025-07-04 22:15:42 +0800

    Remove "All Sites Overview" option from site filtering dropdown in index.vue for improved clarity and usability.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -151,9 +151,6 @@ function handleScheduleMarkAsDone(scheduleId: string) {
             class="px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
             @change="handleSiteChange"
           >
-            <option value="">
-              All Sites Overview
-            </option>
             <option
               v-for="site in sites"
               :key="site.id"
